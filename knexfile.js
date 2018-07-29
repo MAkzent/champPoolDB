1c9a05827a38c3129e4e015b83a230684500a107
MAkzent <arnold.michael91@gmail.com>
2018-07-29 15:13:48 +0900

    initiated DB for heroku

--- knexfile.js
+++ knexfile.js
@@ -14,12 +14,10 @@ module.exports = {
       tableName: "knex_migrations",
     },
   },
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   }
-  // }
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'postgres://siweyqhfgunvjz:ac8a550334e3ecae037038c4a5e409b102f89546edfe11ac2da443d16b037f7c@ec2-23-21-216-174.compute-1.amazonaws.com:5432/d4f93mbkr3s0fb',
+    }
+  }
 };
